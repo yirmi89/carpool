638ffaad7cae9fe6024fccafb513d308537b71dd
yirmi89 <115631964+yirmi89@users.noreply.github.com>
2024-03-25 20:54:41 +0200

    signup and login pages

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:carpool_app/authentication/signup_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget
+{
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: Colors.black,
+      ),
+      home: SignUpScreen(),
+    );
+  }
+}
+
